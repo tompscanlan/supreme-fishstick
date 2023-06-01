3739540cbf139157704705682d19a73446b1ce78
Tom Scanlan <tompscanlan@gmail.com>
2023-06-01 17:07:42 -0400

    update amplify config so lambda can reach db

--- amplify/backend/function/countAllShortUrls/src/main.go
+++ amplify/backend/function/countAllShortUrls/src/main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+
+	tablename := os.Getenv("API_SHORTURL_SHORTURLSTABLE_NAME")
 	sess, err := session.NewSession()
 	if err != nil {
 		fmt.Println("Error getting session:")
@@ -25,26 +27,13 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 
 	svc := dynamodb.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
 
-	keyCondition := expression.Key("id").BeginsWith("a")
-	expr, err := expression.NewBuilder().WithKeyCondition(keyCondition).Build()
-	if err != nil {
-		fmt.Println("Error building expression:")
-		fmt.Println(err)
-
-		return "nil", err
-	}
-
-	input := &dynamodb.QueryInput{
-		TableName:                 aws.String("ShortUrls-f6gjuvmkzzd2fpgp56gqf5e4gm-dev"),
-		Select:                    aws.String(dynamodb.SelectCount),
-		KeyConditionExpression:    expr.KeyCondition(),
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
+	input := &dynamodb.DescribeTableInput{
+		TableName: aws.String(tablename),
 	}
 
 	fmt.Println("input:")
 	fmt.Println(input.String())
-	resp, err := svc.Query(input)
+	resp, err := svc.DescribeTable(input)
 	if err != nil {
 		fmt.Println("failed query")
 		fmt.Println(err)
@@ -53,7 +42,7 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	}
 	fmt.Println(resp)
 
-	return fmt.Sprintf("%d", 16), nil
+	return fmt.Sprintf("%d", *resp.Table.ItemCount), nil
 }
 
 func main() {
